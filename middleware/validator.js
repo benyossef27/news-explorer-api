d3287b74867c193dd9bf8eb0f6b0b9a882582b21
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-07-26 14:45:02 +0300

    Update validator.js

--- middleware/validator.js
+++ middleware/validator.js
@@ -29,7 +29,7 @@ module.exports.validateCreateArticle = celebrate({
 
 module.exports.validateDeleteArticle = celebrate({
   params: Joi.object().keys({
-    id: Joi.string().min().max().required().hex(),
+    articleId: Joi.string().min(24).max(24).required().hex(),
   }),
 });
 
